0fcd24105ebaacd48e81d05118d76cb56935912f
paulmjsong <paulmjsong@gmail.com>
2025-11-05 09:49:54 -0500

    Cleanup

--- graph_construction/extract_entities.py
+++ graph_construction/extract_entities.py
@@ -20,8 +20,8 @@ def extract_data(llm: OpenAILLM, src_path: str, dst_path: str) -> None:
             f"Input data:\n{article}\n\n{CONSTRUCTION_PROMPT}",
         )
         content = json.loads(result.content)
-        extracted['entities'].extend(json['entities'])
-        extracted['relations'].extend(json['relations'])
+        extracted['entities'].extend(content['entities'])
+        extracted['relations'].extend(content['relations'])
     
     with open(dst_path, "a", encoding="utf-8") as dst_file:
         json.dump(extracted, dst_file, ensure_ascii=False, indent=4)
